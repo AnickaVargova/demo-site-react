1ae42814d5ff504a5bcdd26201f9c6a4668ef01d
Anna <anna.vargova@bloomreach.com>
2023-10-30 12:08:16 +0100

    with snippet, with different contents

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import "./App.css";
-import img from "./spring_tree.jpg";
+import img from "./autumn.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Demo website</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        imperdiet ullamcorper urna sed fermentum. Mauris condimentum
-        sollicitudin varius. Ut malesuada a purus non sagittis. Pellentesque
-        vitae nunc vitae massa auctor pulvinar non id eros. Nulla vel rutrum
-        risus. Nunc bibendum eros non ultricies posuere. Quisque ipsum lorem,
-        blandit in lectus egestas, dictum condimentum elit. Phasellus sagittis
-        luctus erat id imperdiet. Phasellus luctus nibh sed mauris tincidunt
-        semper. Proin in fermentum est, dictum vestibulum neque. Ut tincidunt,
-        est pretium pretium facilisis, tortor mauris pellentesque urna, non
-        ultricies neque eros et mauris. Praesent sit amet nulla a felis
-        ultricies iaculis. Aliquam semper ex at justo lobortis iaculis vitae
-        quis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Integer vitae tempor ante. Mauris dictum commodo turpis, a auctor quam
-        mattis sit amet. Aliquam in blandit nunc, at consectetur mauris. Nulla
-        dapibus libero metus, non mollis erat consectetur at. Class aptent
-        taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-        himenaeos. Cras ullamcorper nunc in ipsum dapibus tempor in a elit. Cras
-        elit mauris, ultricies in fringilla vel, dictum at diam. Vivamus rhoncus
-        odio et nisl sagittis elementum. Pellentesque efficitur risus sit amet
-        suscipit faucibus. Suspendisse aliquam feugiat elementum. Vestibulum
-        laoreet tortor tempus enim auctor, et vehicula dolor auctor. Vivamus
-        sodales vehicula lorem, eu sollicitudin ipsum tincidunt eu. Vivamus
-        egestas cursus turpis eu feugiat. Nam metus diam, placerat nec fermentum
-        sit amet, venenatis quis dolor. Nunc in vulputate dolor. In hac
-        habitasse platea dictumst. Morbi pulvinar sem elit, non vehicula tortor
-        ullamcorper mollis. In vitae risus pulvinar, lobortis elit sit amet,
-        accumsan magna. Donec vehicula velit nunc, a mattis est auctor et. Lorem
-        ipsum dolor sit amet, consectetur adipiscing elit. In ex velit, tempor
-        vitae sem a, scelerisque viverra sem. Morbi mattis posuere nisl, ac
-        fringilla est semper nec. Donec ullamcorper enim sed mi consectetur
-        vestibulum. Praesent imperdiet, massa cursus aliquet interdum, ex odio
-        egestas elit, in fringilla nisl velit elementum purus. Mauris varius
-        enim velit, eu mollis lacus congue in. Curabitur sed condimentum erat,
-        ac ornare nulla. Nunc hendrerit lacinia purus sed convallis. Cras mauris
-        ante, tincidunt at gravida in, porta id lorem. Curabitur dictum sodales
-        metus, ut scelerisque ante imperdiet non. Morbi tincidunt auctor nisl,
-        id placerat eros efficitur ac. Nunc justo lacus, mattis at elit
-        pulvinar, lacinia egestas libero. Nulla nisl metus, posuere vel lacus
-        eu, elementum tristique nisl. Vivamus eu tellus leo. Vestibulum porta
-        sem et enim euismod, ac tempus nulla vehicula. Cras eu volutpat felis.
-        Vivamus eget mollis neque. Vestibulum at nisi purus.
+        Suspendisse orci nisi, semper at lectus lacinia, luctus dignissim enim.
+        Ut eget tincidunt sem. In aliquet consequat metus in vestibulum. Sed
+        tellus massa, porta sed aliquam quis, euismod vel nibh. Proin interdum
+        viverra tortor. Sed dapibus cursus tortor, eget hendrerit est tincidunt
+        ut. Integer molestie, massa in tristique porttitor, nibh nunc auctor
+        tellus, egestas sagittis ligula velit ut tellus. Quisque et aliquet
+        quam. Suspendisse ut fermentum tellus. Donec tempor eget magna et
+        rhoncus. Pellentesque molestie consequat metus et dapibus. Aenean
+        vehicula tristique aliquet. Nam vestibulum feugiat leo vel pellentesque.
+        Suspendisse potenti. Maecenas sit amet elit augue. Ut sodales ut velit
+        eu ultricies. Cras tincidunt nunc metus, consectetur pulvinar tellus
+        lacinia sed. Ut ut laoreet augue, ut ullamcorper orci. Ut sit amet
+        aliquet purus, non rutrum ligula. Proin vitae risus ac orci viverra
+        blandit. Vivamus bibendum gravida ex blandit tempor. Integer sit amet
+        pellentesque dolor. Cras eleifend enim eu purus efficitur, nec
+        scelerisque erat consectetur. Maecenas et diam sed mauris facilisis
+        consequat. Curabitur auctor orci ac ante maximus pharetra. Curabitur
+        consequat eget velit aliquet ultricies. Sed est leo, facilisis sed nisi
+        id, aliquam ultricies dui. Nam facilisis, dolor in efficitur commodo,
+        lectus quam maximus arcu, in condimentum mauris purus quis diam. Integer
+        non leo dictum, consequat leo ut, porta mauris. Duis risus justo,
+        blandit ac neque id, ornare vehicula velit. Aliquam porta ultrices justo
+        vitae molestie. Fusce vestibulum lectus non sapien rhoncus pretium.
       </p>
-      <img src={img} alt="tree" />
+      <img src={img} alt="autumn" />
     </div>
   );
 }
